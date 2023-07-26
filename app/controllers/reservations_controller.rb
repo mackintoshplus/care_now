0e5f9f3c6b4f299b20a29b73564e8cf577d21286
Hisaaki Komaki <h-komaki@qg7.so-net.ne.jp>
2023-07-26 08:16:52 +0000

    first commit

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,23 @@
+class ReservationsController < ApplicationController
+  def index
+    @current_reservations = Reservation.where(date: Date.today).count
+  end
+
+  def new
+    @current_reservations = Reservation.where(date: Date.today).count
+  end
+
+  def create
+    reservation = Reservation.new(date: Date.today)
+  
+    if reservation.save
+      redirect_to reservations_path, notice: '予約しました'
+    else
+      redirect_to new_reservation_path, alert: '予約に失敗しました'
+    end
+  end
+
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Reservations routes
+  resources :reservations, only: [:index, :new, :create, :destroy]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Root path route
+  root "reservations#index"
 end
